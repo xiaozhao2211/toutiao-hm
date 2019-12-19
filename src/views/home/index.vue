f8b98b78ade1a1845e15a6e7a389ad153884f6e4
zhaodongxue <1510386905@qq.com>
2019-12-19 20:59:28 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,16 @@
 <template>
-<div>主页</div>
+<!-- 外层容器 -->
+<el-container>
+    <!-- 左侧边栏导航 -->
+    <el-aside>左侧边栏导航</el-aside>
+    <!-- 右侧内容 -->
+    <el-container>
+        <!-- 头部 -->
+        <el-header>头</el-header>
+        <el-main>body</el-main>
+    </el-container>
+</el-container>
+
 </template>
 
 <script>
