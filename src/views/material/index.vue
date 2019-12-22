2b3a27a5454f67d5e2098248eaf8cc592e0a7ed6
zhaodongxue <1510386905@qq.com>
2019-12-22 21:55:12 +0800

    素材列表-新建页面-挂载路由

---
+++ src/views/material/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-card>
+      <bread-crumb slot="header">
+      <!-- 插槽内容 -->
+      <template slot="title">素材管理</template>
+      </bread-crumb>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
