91e9b59f5bf4410f7db76eeede67d792b2ea9671
zhaodongxue <1510386905@qq.com>
2019-12-23 19:46:22 +0800

    素材列表-素材的上传

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
       <bread-crumb slot="header">
       <!-- 插槽内容 -->
       <template slot="title">素材管理</template>
       </bread-crumb>
+      <!-- 上传图片 -->
+      <el-row type='flex' justify="end">
+        <el-upload action="" :http-request="uploadImg" :show-file-list="false">
+          <el-button size="small" type="primary" >上传图片</el-button>
+        </el-upload>
+      </el-row>
+      <!-- 标签页 -->
         <el-tabs v-model="activeName" @tab-click="changeTag">
+          <!-- 标签 -->
           <el-tab-pane label="全部图片" name="all">
+            <!-- 生成页面结构  v-for 循环-->
             <div class="img-list">
               <el-card v-for="item in list"  :key="item.id" class="img-card">
                 <img :src="item.url" alt="">
@@ -24,6 +33,7 @@
             </div>
           </el-tab-pane>
         </el-tabs>
+         <!-- 公共分页组件 -->
         <el-row type="flex" justify="center" align="middle" style="height:60px">
          <el-pagination background layout="prev, pager, next"
           :total="page.total"
@@ -39,34 +49,51 @@
 export default {
   data () {
     return {
-      activeName: 'all',
-      list: [],
+      activeName: 'all', // 当前选中的标签
+      list: [], // 接收素材数据
       page: {
         pageSize: 8,
         currentPage: 1,
         total: 0
-      }
+      },
+      loading: false
     }
   },
   methods: {
+    // 上传图片方法
+    uploadImg (params) {
+      this.loading = true
+      let data = new FormData()
+      data.append('image', params.file)
+      this.$axios({
+        url: '/user/images',
+        method: 'post',
+        data
+      }).then(result => {
+        this.loading = false
+        this.getMaterial()
+      })
+    },
+    // 改变页码方法
     changePage (newPage) {
       this.page.currentPage = newPage
       this.getMaterial()
     },
+    // 切换页签方法
     changeTag () {
-      this.page.currentPage = 1
+      this.page.currentPage = 1// 重置回第一页
       this.getMaterial()
     },
     // 获取素材方法
     getMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect',
+        params: { collect: this.activeName === 'collect', // 传false是获取所有的数据 传true是获取收藏数据
           page: this.page.currentPage,
           per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
-        this.page.total = result.data.total_count
+        this.page.total = result.data.total_count // 总条数
       })
     }
   },
@@ -82,7 +109,7 @@ export default {
   flex-wrap: wrap;
   .img-card {
       width: 200px;
-      height: 250px;
+      height: 220px;
       margin: 20px 30px;
       position: relative;
     img {
