9c5feaed921a4dd31a92bb0c636ca3213d2ac73f
zhaodongxue <1510386905@qq.com>
2019-12-25 00:17:31 +0800

    内容列表-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -35,7 +35,7 @@
       <!-- 内容列表-页面主体结构 -->
       <el-card>
         <el-row class='total' type="flex" align="middle">
-          <span>共找到n条符合条件的内容</span>
+          <span>共找到{{page.total}}条符合条件的内容</span>
         </el-row>
         <div class="article-item" v-for="item in list" :key="item.id.toString()">
            <div class="left">
@@ -53,6 +53,14 @@
               <span><i class="el-icon-delete"></i>删除</span>
            </div>
         </div>
+        <el-row type="flex" justify="center" align="middle" style="height:60px">
+         <el-pagination background layout="prev, pager, next"
+          :total="page.total"
+          :page-size="page.pageSize"
+          :current-page="page.currentPage"
+          @current-change="changePage">
+         </el-pagination>
+       </el-row>
       </el-card>
   </el-card>
 </template>
@@ -68,7 +76,12 @@ export default {
       },
       channels: [], // 接收频道
       list: [], // 接收文章数据
-      defaultImg: require('../../assets/img/header.jpg')
+      defaultImg: require('../../assets/img/header.jpg'),
+      page: {
+        pageSize: 10,
+        currentPage: 1,
+        total: 0
+      }
     }
   },
   filters: {
@@ -103,9 +116,21 @@ export default {
     }
   },
   methods: {
-    // 选择变化监听
+    // 页码改变
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getChangeCondition()// 拉取文章数据
+    },
+    // 条件改变
     changeCondition () {
+      this.page.currentPage = 1
+      this.getChangeCondition()
+    },
+    // 选择变化监听
+    getChangeCondition () {
       let params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize,
         status: this.searchForm.status === 5 ? null : this.searchForm.status, // 状态为5就传，否则传当前状态值
         channel_id: this.searchForm.channel_id,
         begin_pubdate: this.searchForm.dateRange.length ? this.searchForm.dateRange[0] : null, // 开始时间
@@ -120,6 +145,7 @@ export default {
         params
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count // 总数
       })
     },
     // 获取文章所有频道
@@ -133,7 +159,7 @@ export default {
   },
   created () {
     this.getChannels()// 拉取频道数据
-    this.getArticles()// 拉取文章数据
+    this.getArticles({ page: 1, per_page: 10 })// 拉取文章数据
   }
 }
 </script>
