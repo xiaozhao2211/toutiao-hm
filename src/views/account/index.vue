f0b2c440e90ac17ca6419da50d13d517636f0d18
zhaodongxue <1510386905@qq.com>
2019-12-28 11:56:04 +0800

    账户信息-用户数据保存-头像上传更新

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,29 +1,35 @@
 <template>
-<el-card>
+  <el-card>
     <bread-crumb slot="header">
       <template slot="title">账户信息</template>
     </bread-crumb>
-    <el-form label-width="80px" style="margin-left:50px;">
-        <el-form-item label="用户名">
-            <el-input style="width:40%" v-model="formData.name"></el-input>
-        </el-form-item>
-        <el-form-item label="简介">
-            <el-input style="width:40%" v-model="formData.intro"></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱">
-            <el-input style="width:40%" v-model="formData.email"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号">
-            <el-input style="width:40%" v-model="formData.mobile"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary">保存信息</el-button>
-        </el-form-item>
+    <el-form
+      label-width="80px"
+      style="margin-left:50px;"
+      :model="formData"
+      :rules="infoRules"
+      ref="myForm"
+    >
+      <el-form-item label="用户名" prop="name">
+        <el-input style="width:40%" v-model="formData.name"></el-input>
+      </el-form-item>
+      <el-form-item label="简介">
+        <el-input style="width:40%" v-model="formData.intro"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input style="width:40%" v-model="formData.email"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input style="width:40%" v-model="formData.mobile" disabled></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="saveUserInfo">保存信息</el-button>
+      </el-form-item>
     </el-form>
-    <el-upload class="upload-head">
-        <img :src="formData.photo?formData.photo:defaultImg" alt="">
+    <el-upload class="upload-head" action :show-file-list="false" :http-request="uploadImg">
+      <img :src="formData.photo?formData.photo:defaultImg" alt />
     </el-upload>
-</el-card>
+  </el-card>
 </template>
 
 <script>
@@ -37,10 +43,44 @@ export default {
         mobile: '',
         photo: ''
       },
-      defaultImg: require('../../assets/img/home_img.jpg')
+      defaultImg: require('../../assets/img/home_img.jpg'),
+      infoRules: {
+        name: [{ required: true, message: '用户名不能为空' },
+          { min: 3, max: 8, message: '用户名长度应为1~8个字' }],
+        email: [{ required: true, message: '邮箱不能为空' },
+          { pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+            message: '邮箱格式不正确' }]
+      }
     }
   },
   methods: {
+    // 上传头像方法
+    uploadImg (params) {
+      let data = new FormData()
+      data.append('photo', params.file)
+      this.$axios({
+        url: '/user/photo',
+        method: 'patch',
+        data
+      }).then(result => {
+        this.formData.photo = result.data.photo
+      })
+    },
+    // 存储用户信息方法(手动校验)
+    saveUserInfo () {
+      this.$refs.myForm.validate().then(result => {
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(result => {
+          this.$message({
+            type: 'success',
+            message: '保存用户信息成功'
+          })
+        })
+      })
+    },
     // 获取用户信息方法
     getUserInfo () {
       this.$axios({
@@ -58,13 +98,13 @@ export default {
 
 <style lang='less' scoped>
 .upload-head {
-    position: absolute;
-    right: 180px;
-    top:160px;
-    img {
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-    }
+  position: absolute;
+  right: 180px;
+  top: 160px;
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+  }
 }
 </style>
