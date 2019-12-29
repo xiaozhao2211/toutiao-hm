9ec136a8eee520558d1652107df7ffa0b6ae2241
zhaodongxue <1510386905@qq.com>
2019-12-29 17:37:15 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,5 +1,8 @@
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  progress.start()// 开启进度条
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -10,4 +13,7 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(() => {
+  progress.done()
 })
